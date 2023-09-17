2b3b3b85097d71da23857d71337160a4044e6dda
jottaxwds <hello@jottax.xyz>
2023-09-18 00:09:28 +0200

    setup apollo provider

---
+++ src/services/ApolloAppProvider.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+export const API_TOKEN = process.env.REACT_APP_API_TOKEN
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${API_TOKEN}`
+  },
+  cache: new InMemoryCache()
+})
+
+const ApolloAppProvider = ({ children }: { children: React.ReactNode }) => (
+  <ApolloProvider client={client}>{children}</ApolloProvider>
+)
+
+export default ApolloAppProvider
